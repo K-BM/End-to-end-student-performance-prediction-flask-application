5c07ae0dfe8d4e4d0e82d3ecd2f8060b8dabeb6e
Karim <karim.boualem@edf.fr>
2024-05-29 16:14:51 +0200

    updated front web app

--- app.py
+++ app.py
@@ -9,13 +9,12 @@ application=Flask(__name__)
 
 app=application
 
-## Route for a home page
-
+# Route for a home page
 @app.route('/')
 def index():
     return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predict_score',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -31,13 +30,9 @@ def predict_datapoint():
 
         )
         pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
 
         predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
         return render_template('home.html',results=results[0])
     
 if __name__ == "__main__":
